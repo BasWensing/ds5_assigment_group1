636135073ebbd6f9e0d733370d42193a2f2abc18
DennisT2A <498090@student.fontys.nl>
2026-09-14 12:53:21 +0200

    Dennis2.py bewerkt
    Variabelen aangeroepen en functie aangemaakt voor Mandelbrot-visualisatie

--- Uitwerkingen/Week2/Dennis.py
+++ Uitwerkingen/Week2/Dennis.py
@@ -1,9 +1,35 @@
+# Opdracht 1
+"""Print the average grade. Dennis"""
+print(f"Average Grade: {average}")
+
+"""Print lines. Dennis"""
+print("--------------------")
+
+'''Add each record to 'filtered_records' in the df 'Records' if the number is higher than 80.0. Dennis'''
+filtered_records = [record for record in records if float(record['Grade']) >= 80.0]
+
+'''Print "Student Report". Dennis '''
+print("Student Report")
+
+'''Print lines. Dennis'''
+print("--------------")
+
+
+for record in filtered_records:
+    '''For each record, print name. Dennis'''
+    print(f"Name: {record['Name']}")
+
+    '''For each record, print grade. Dennis'''
+    print(f"Grade: {record['Grade']}")
+
+    '''Print lines. Dennis'''
+    print("--------------------")
+
 # Opdracht 2
 # Reeks toevoegen met getallen: An,c, A0 moeten erin. 
 # Reeks moet binnen de [-1.5 0.5] voor de x en [-1 1] voor de y. Dennis.
 import pandas as pd
 import numpy as np
-
 
 # Stap 1: Sequence toevoegen
 x_min = -1.5
@@ -12,9 +38,12 @@ y_min = -1
 y_max = 1
 n = 10000 # Aantal gegenereerde formulewaarden op interval [x_min, x_max]
 c = x + y*i # Complexe set
-a(index) = (a(index-1))**2 + c # Mandelbrot set
+index = 0 # Index itereren over lijst met functiewaarden
 
+# Stap 2: lijst aanmaken voor functiewaarden
+mandelbrot_numbers = []
 
-# Stap 2: sequence genereren van 10000 getallen in range x = [-1.5, 0.5]
-for x in range(x_min, x_max, n):
-    print(x)
+# Stap 3: sequence genereren van 10000 getallen in range x = [-1.5, 0.5]
+for index in range(x_min, x_max, n):
+    mandelbrot_numbers.append(mandelbrot_numbers[index-1])**2 + c # Mandelbrot set
+    
