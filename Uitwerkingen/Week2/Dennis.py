033e41c59dc567cc0c0b63b0ee60cfcc96b9eb52
DennisT2A <498090@student.fontys.nl>
2026-09-14 12:39:52 +0200

    Dennis2.ipynb verwijderd, Dennis2.py aangemaakt

---
+++ Uitwerkingen/Week2/Dennis.py
@@ -0,0 +1,20 @@
+# Opdracht 2
+# Reeks toevoegen met getallen: An,c, A0 moeten erin. 
+# Reeks moet binnen de [-1.5 0.5] voor de x en [-1 1] voor de y. Dennis.
+import pandas as pd
+import numpy as np
+
+
+# Stap 1: Sequence toevoegen
+x_min = -1.5
+x_max = 0.5
+y_min = -1 
+y_max = 1
+n = 10000 # Aantal gegenereerde formulewaarden op interval [x_min, x_max]
+c = x + y*i # Complexe set
+a(index) = (a(index-1))**2 + c # Mandelbrot set
+
+
+# Stap 2: sequence genereren van 10000 getallen in range x = [-1.5, 0.5]
+for x in range(x_min, x_max, n):
+    print(x)
