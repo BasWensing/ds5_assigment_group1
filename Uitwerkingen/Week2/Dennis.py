dd5321a91b11b394a67d4ccf53eb9bfa2b476472
DennisT2A <498090@student.fontys.nl>
2026-09-16 13:43:20 +0200

    Bestand Dennis bijgewerkt

--- Uitwerkingen/Week2/Dennis.py
+++ Uitwerkingen/Week2/Dennis.py
@@ -1,49 +1,34 @@
-# Opdracht 1
-"""Print the average grade. Dennis"""
-print(f"Average Grade: {average}")
-
-"""Print lines. Dennis"""
-print("--------------------")
-
-'''Add each record to 'filtered_records' in the df 'Records' if the number is higher than 80.0. Dennis'''
-filtered_records = [record for record in records if float(record['Grade']) >= 80.0]
-
-'''Print "Student Report". Dennis '''
-print("Student Report")
-
-'''Print lines. Dennis'''
-print("--------------")
-
-
-for record in filtered_records:
-    '''For each record, print name. Dennis'''
-    print(f"Name: {record['Name']}")
-
-    '''For each record, print grade. Dennis'''
-    print(f"Grade: {record['Grade']}")
-
-    '''Print lines. Dennis'''
-    print("--------------------")
-
 # Opdracht 2
 # Reeks toevoegen met getallen: An,c, A0 moeten erin. 
 # Reeks moet binnen de [-1.5 0.5] voor de x en [-1 1] voor de y. Dennis.
-import pandas as pd
-import numpy as np
 
-# Stap 1: Sequence toevoegen
+import numpy as np
+import pandas as pd
+c = [] # maakt een lijst aan voor alle c's die we nodig hebben
+def complex(x,y): # Definieert de complex functie   
+    c.append(x + y*1j) # Rekent de x + y*i uit en voegt deze toe aan een list
+
+
+import matplotlib.pyplot as plt
+def draw_mandel(width): # Definieert hoe groot de afbeelding moet zijn
+    plt.figure(figsize =(width,width)) # Definieert hoe groot de afbeelding moet zijn volgens en past dit toe and de figure
+
+# Stap 1.1 parameters toevoegen
 x_min = -1.5
 x_max = 0.5
 y_min = -1 
 y_max = 1
-n = 10000 # Aantal gegenereerde formulewaarden op interval [x_min, x_max]
-c = x + y*i # Complexe set
+n = 10 # Aantal gegenereerde formulewaarden op interval [x_min, x_max]
+a_n = a_n-1+c
+a0 = 0
+step = abs((x_max - x_min) / n)
+print(step)
 index = 0 # Index itereren over lijst met functiewaarden
 
-# Stap 2: lijst aanmaken voor functiewaarden
-mandelbrot_numbers = []
+# Stap 1.2: sequence van n = 1000000 getallen 
+x = np.arange(x_min, (x_max+step), step)
+print(x)
 
-# Stap 3: sequence genereren van 10000 getallen in range x = [-1.5, 0.5]
-for index in range(x_min, x_max, n):
-    mandelbrot_numbers.append(mandelbrot_numbers[index-1])**2 + c # Mandelbrot set
-    
+# Stap 2: sequence genereren van 10000 getallen in range x = [-1.5, 0.5]
+for i in x:
+
