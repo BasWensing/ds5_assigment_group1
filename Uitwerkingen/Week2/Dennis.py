11323c55a551588f0b0fa73853d0131f68b85eb0
DennisT2A <498090@student.fontys.nl>
2026-09-16 16:18:59 +0200

    Update Dennis.py

--- Uitwerkingen/Week2/Dennis.py
+++ Uitwerkingen/Week2/Dennis.py
@@ -10,44 +10,43 @@ import matplotlib.pyplot as plt
 def draw_mandel(width): # Definieert hoe groot de afbeelding moet zijn
     plt.figure(figsize =(width,width)) # Definieert hoe groot de afbeelding moet zijn volgens en past dit toe and de figure
 
-import numpy as np
-c = [] # maakt een lijst aan voor alle c's die we nodig hebben
 def complex(x,y): # Definieert de complex functie   
+    index1 = 0
     for x_value in x: # Pakt alle x van de x lijst
         for y_value in y: # Pakt alle y van de y lijst
-            c.append(x_value + y_value * 1j) # Rekened de x + y*i uit en voegt deze toe aan een list 
+            c.append(x_value + y_value * 1j) # Rekened de x + y*i uit en voegt deze toe aan een list
+            print(c[index1])
+            index1 += 1
+x = []
+y = []
 
 complex(x,y)
 len(c)
 print(c)
 
-import numpy as np
-
-x = []
-y =[]
-
-def generate_x_en_y(width): # Genereert de benodige x en y
-    x.extend(np.linspace(-1.5, 1,width)) # Maakt alle benodige x   
-
-    y.extend(np.linspace(-1, 1, width)) # Maakt alle benodige y
-generate_x_en_y(200)
-
-# Stap 1.1 parameters toevoegen
 x_min = -1.5
 x_max = 0.5
 y_min = -1 
 y_max = 1
+
+width = 200
+def generate_x_en_y(width): # Genereert de benodige x en y
+    x.extend(np.linspace(x_min, x_max, width)) # Maakt alle benodige x   
+
+    y.extend(np.linspace(y_min, y_max, width)) # Maakt alle benodige y
+generate_x_en_y(width)
+
+# Stap 1.1 parameters toevoegen
+
 n = 10 # Aantal gegenereerde formulewaarden op interval [x_min, x_max]
-a_n = a_n-1+c
+a[i] = [(a[i-1])**2 + c]
+
 a0 = 0
 step = abs((x_max - x_min) / n)
 print(step)
 index = 0 # Index itereren over lijst met functiewaarden
 
-# Stap 1.2: sequence van n = 1000000 getallen 
-x = np.arange(x_min, (x_max+step), step)
-print(x)
-
 # Stap 2: sequence genereren van 10000 getallen in range x = [-1.5, 0.5]
-
-
+index2 = 0
+for i in c:
+    a0 = (a0(i-1))**2 + c
