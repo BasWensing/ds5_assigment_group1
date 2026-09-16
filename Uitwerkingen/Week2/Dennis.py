3e4a7b9c9cc18ba97f3fb2710835a34d8b4f7f33
DennisT2A <498090@student.fontys.nl>
2026-09-16 14:31:27 +0200

    Bestand Dennis.py bijgewerkt

--- Uitwerkingen/Week2/Dennis.py
+++ Uitwerkingen/Week2/Dennis.py
@@ -5,13 +5,32 @@
 import numpy as np
 import pandas as pd
 c = [] # maakt een lijst aan voor alle c's die we nodig hebben
-def complex(x,y): # Definieert de complex functie   
-    c.append(x + y*1j) # Rekent de x + y*i uit en voegt deze toe aan een list
-
 
 import matplotlib.pyplot as plt
 def draw_mandel(width): # Definieert hoe groot de afbeelding moet zijn
     plt.figure(figsize =(width,width)) # Definieert hoe groot de afbeelding moet zijn volgens en past dit toe and de figure
+
+import numpy as np
+c = [] # maakt een lijst aan voor alle c's die we nodig hebben
+def complex(x,y): # Definieert de complex functie   
+    for x_value in x: # Pakt alle x van de x lijst
+        for y_value in y: # Pakt alle y van de y lijst
+            c.append(x_value + y_value * 1j) # Rekened de x + y*i uit en voegt deze toe aan een list 
+
+complex(x,y)
+len(c)
+print(c)
+
+import numpy as np
+
+x = []
+y =[]
+
+def generate_x_en_y(width): # Genereert de benodige x en y
+    x.extend(np.linspace(-1.5, 1,width)) # Maakt alle benodige x   
+
+    y.extend(np.linspace(-1, 1, width)) # Maakt alle benodige y
+generate_x_en_y(200)
 
 # Stap 1.1 parameters toevoegen
 x_min = -1.5
@@ -30,5 +49,5 @@ x = np.arange(x_min, (x_max+step), step)
 print(x)
 
 # Stap 2: sequence genereren van 10000 getallen in range x = [-1.5, 0.5]
-for i in x:
+
 
