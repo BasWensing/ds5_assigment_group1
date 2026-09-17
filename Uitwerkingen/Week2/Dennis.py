08255a00a88ec0eb6dd2025c33f3fa5a1f231292
DennisT2A <498090@student.fontys.nl>
2026-09-17 21:50:44 +0200

    Mandelbrot-sequentie toegevoegd en kleuren afbeelding aangepast

--- Uitwerkingen/Week2/Dennis.py
+++ Uitwerkingen/Week2/Dennis.py
@@ -7,46 +7,81 @@ import pandas as pd
 c = [] # maakt een lijst aan voor alle c's die we nodig hebben
 
 import matplotlib.pyplot as plt
+from matplotlib.colors import LinearSegmentedColormap
+
+
 def draw_mandel(width): # Definieert hoe groot de afbeelding moet zijn
     plt.figure(figsize =(width,width)) # Definieert hoe groot de afbeelding moet zijn volgens en past dit toe and de figure
 
-def complex(x,y): # Definieert de complex functie   
-    index1 = 0
-    for x_value in x: # Pakt alle x van de x lijst
-        for y_value in y: # Pakt alle y van de y lijst
-            c.append(x_value + y_value * 1j) # Rekened de x + y*i uit en voegt deze toe aan een list
-            print(c[index1])
-            index1 += 1
+width = 200
 x = []
 y = []
-
-complex(x,y)
-len(c)
-print(c)
 
 x_min = -1.5
 x_max = 0.5
 y_min = -1 
 y_max = 1
 
-width = 200
 def generate_x_en_y(width): # Genereert de benodige x en y
     x.extend(np.linspace(x_min, x_max, width)) # Maakt alle benodige x   
 
     y.extend(np.linspace(y_min, y_max, width)) # Maakt alle benodige y
 generate_x_en_y(width)
 
+def complex(x,y): # Definieert de complex functie   
+    index1 = 0
+    for x_value in x: # Pakt alle x van de x lijst
+        for y_value in y: # Pakt alle y van de y lijst
+            c.append(x_value + y_value * 1j) # Rekened de x + y*i uit en voegt deze toe aan een list
+            index1 += 1
+
+complex(x,y)
+len(c)
+
 # Stap 1.1 parameters toevoegen
 
-n = 10 # Aantal gegenereerde formulewaarden op interval [x_min, x_max]
-a[i] = [(a[i-1])**2 + c]
-
+n = 10 # Aantal gegenereerde formulewaarden op interval [x_min, x_max
 a0 = 0
 step = abs((x_max - x_min) / n)
-print(step)
 index = 0 # Index itereren over lijst met functiewaarden
 
-# Stap 2: sequence genereren van 10000 getallen in range x = [-1.5, 0.5]
-index2 = 0
-for i in c:
-    a0 = (a0(i-1))**2 + c
+print(c)
+sequence_mandelbrot = []
+max_stappen = 100
+# Stap 2: sequence genereren van n getallen in range x = [-1.5, 0.5]
+for c_waarde in c:
+    an_oud = 0  # Op 0 beginnen voor elk nieuw complex getal
+    n = 0
+    # oude waarde checken
+    while abs(an_oud) <= 2 and n < max_stappen:
+        an_nieuw = an_oud**2 + c_waarde  # Nieuwe waarde berekenen
+        an_oud = an_nieuw                # Update an_oud voor volgende stap
+        n += 1 # n = n+1
+    sequence_mandelbrot.append(abs(an_oud)) # Afbeelding 90 graden draaien
+print(sequence_mandelbrot)
+
+# figuur tekenen met functie draw_mandel
+def draw_mandel_heatmap(width):
+    grid = np.array(sequence_mandelbrot).reshape(width,width)# figuur met 90 graden naar links draaien
+    grid_gedraaid = np.rot90(grid) # figuur met 90 graden naar links draaien
+    plt.figure(figsize =(8,6))
+    plt.title('Visualisatie van Mandelbrot (Heatmap)',fontsize=20)
+    kleuren = LinearSegmentedColormap.from_list(
+        "zwart_blauw",
+        ["black", "blue"])
+
+    plt.imshow(
+        grid_gedraaid,
+        cmap=kleuren,
+        vmin=0,
+        vmax=2
+    )
+
+    plt.colorbar(label="Waarde |aₙ|")
+    plt.xlabel('x')
+    plt.ylabel('y')
+
+    plt.tight_layout()
+    plt.show()
+
+draw_mandel_heatmap(200)
